8bfeb72329af3ceb039ba7c316bf96595a4016ec
Hardik Jain <hardikpalrechaHP@gmail.com>
2025-01-27 01:27:22 +0530

    Project setup with backend and front end

---
+++ server/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
+
+//Configuration
+dotenv.config(); //loads the environment variable
+const app = express(); //loads the express object to build the web server
+app.use(express.json()); //parse the incoming url in json
+app.use(helmet()); //used for securing the headers of express application
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" })); //Blocks others from loading your resources cross-origin
+app.use(morgan("common")); //generates the logs of incoming requests.
+app.use(bodyParser.json()); // parse the incoming requests to json, url-encoded, text or etc.
+app.use(bodyParser.urlencoded({ extended: false })); //prases bodies from URL.
+app.use(cors()); //prevents from unauthorize access from different domains
+
+// Routes
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", managementRoutes);
+app.use("/sales", salesRoutes);
+
+//MongoDB setup
+const PORT = process.env.PORT || 9000;
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server listening on PORT: ${PORT}`));
+  })
+  .catch((error) => console.log(error));
