2fe0287d72db76dfd0fa8d87b7b4f66fbbffc8aa
Hardik Jain <hardikpalrechaHP@gmail.com>
2025-01-29 13:46:21 +0530

    Navbar left side

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { Box } from "@mui/material";
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
+import NavBar from '../../components/NavBar';
 
 const Layout = () => {
   return (
-    <div>Layout</div>
-  )
-}
+    <Box width="100%" height="100%">
+      <Box>
+        <NavBar />
+        <Outlet />
+      </Box>
+    </Box>
+  );
+};
 
-export default Layout
+export default Layout;
